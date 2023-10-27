4b0e8b8e2c44b8176b9616a1475c4408e2b6e24d
Tibor Mach <tibor@iterative.ai>
2023-10-27 11:10:16 +0200

    experiment

--- create_plot.py
+++ create_plot.py
@@ -7,3 +7,13 @@ with Live() as live:
         live.log_metric("acc", i + random() - 0.5)
 
         live.next_step()
+
+    y_true = [0, 0, 1, 1]
+    y_pred = [1, 0, 1, 0]
+
+    live.log_sklearn_plot(
+        kind="confusion_matrix",
+        labels=y_true,
+        predictions=y_pred,
+        name="confusion_matrix.json",
+        )
